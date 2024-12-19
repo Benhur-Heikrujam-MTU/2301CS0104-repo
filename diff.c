c1c1d6fe9dc35001667235f2efaeb20eadc521e7
Benhur <benhur_h23@mtu.ac.in>
2024-12-19 10:47:22 +0530

    committed sum.c and diff.c

---
+++ diff.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a,b;
+printf("Enter two integers:\n");
+scanf("%d %d",&a,&b);
+printf("The difference of %d and %d is %d",a,b,a-b);
+printf("\nEnd of program");
+}
